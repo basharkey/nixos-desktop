ac6b06b20c076ee3b1cd74f13597bb0fffb27605
System administrator <15859442+basharkey@users.noreply.github.com>
2022-07-03 12:08:12 -0400

    passthrough post script

--- libvirt.nix
+++ libvirt.nix
@@ -23,45 +23,24 @@ with lib;
 
   # restart libvirtd to apply any configuration changes
   systemd.services.libvirtd = {
+    # add bash to env
+    # https://github.com/NixOS/nixpkgs/issues/51152
+    path = let
+      env = pkgs.buildEnv {
+        name = "qemu-hook-env";
+        paths = with pkgs; [
+          bash
+        ];
+      };
+    in [ env ];
     # create qemu hook helper script with proper bash location
     # https://github.com/PassthroughPOST/VFIO-Tools
     preStart =
       let
-        hookHelper = pkgs.writeScriptBin "qemu" ''
-          #!${pkgs.stdenv.shell}
-          #
-          # Author: Sebastiaan Meijer (sebastiaan@passthroughpo.st)
-          #
-          # Copy this file to /etc/libvirt/hooks, make sure it's called "qemu".
-          # After this file is installed, restart libvirt.
-          # From now on, you can easily add per-guest qemu hooks.
-          # Add your hooks in /etc/libvirt/hooks/qemu.d/vm_name/hook_name/state_name.
-          # For a list of available hooks, please refer to https://www.libvirt.org/hooks.html
-          #
-          
-          GUEST_NAME="$1"
-          HOOK_NAME="$2"
-          STATE_NAME="$3"
-          MISC="''${@:4}"
-          
-          BASEDIR="$(dirname $0)"
-          
-          HOOKPATH="$BASEDIR/qemu.d/$GUEST_NAME/$HOOK_NAME/$STATE_NAME"
-          
-          set -e # If a script exits with an error, we should as well.
-          
-          # check if it's a non-empty executable file
-          if [ -f "$HOOKPATH" ] && [ -s "$HOOKPATH"] && [ -x "$HOOKPATH" ]; then
-              eval \"$HOOKPATH\" "$@"
-          elif [ -d "$HOOKPATH" ]; then
-              while read file; do
-                  # check for null string
-                  if [ ! -z "$file" ]; then
-                    eval \"$file\" "$@"
-                  fi
-              done <<< "$(find -L "$HOOKPATH" -maxdepth 1 -type f -executable -print;)"
-          fi
-        '';
+        hookHelper = pkgs.writeScriptBin "qemu" (builtins.readFile (pkgs.fetchurl {
+          url = "https://raw.githubusercontent.com/PassthroughPOST/VFIO-Tools/master/libvirt_hooks/qemu";
+          sha256 = "a0b3a8754d8fb4eee75faed6f0fb9f9683d86a6f5acb2d305991a3625ff76d60";
+        }));
       in
         ''  
           mkdir -p /var/lib/libvirt/hooks
